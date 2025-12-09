7f4bd7f2180630a2639eb7b519599e8213ee0c16
Your Name <you@example.com>
2025-12-09 11:41:19 +0600

    add function

---
+++ JsProblemSolving/ES6/addFunctuon.js
@@ -0,0 +1,13 @@
+// QN–001: Function to Add Two Numbers
+
+// This function takes two numbers as parameters
+// and returns their sum.
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Calling the function with sample values
+const result = addNumbers(10, 20);
+
+// Printing the result
+console.log(result);  // Output: 30
